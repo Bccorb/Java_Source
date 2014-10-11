efc24ea9b0131efca7ab20497bfd031fbc5bbb84
Brandon Corbett <bccorb1000@gmail.com>
2014-10-11 17:47:03 -0400

    Create QueueADT.java

---
+++ CircularArrayQueue/QueueADT.java
@@ -0,0 +1,15 @@
+public interface QueueADT<T> 
+{
+
+	public void enqueue(T element);
+	
+	public T dequeue();
+	
+	public T first();
+	
+	public boolean isEmpty();
+	
+	public int size();
+	
+	public String toString();
+}
